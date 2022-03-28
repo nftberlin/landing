eb168e69c874de5ba879f92a852c14932bdd9ef6
Lulu <60514664+doclew@users.noreply.github.com>
2022-03-28 10:12:30 +0200

    commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './themes/style.scss'
+import './themes/resp.scss'
+import './fonts/stylesheet.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
+
+Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper,
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app'))
