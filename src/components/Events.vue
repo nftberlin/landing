e2e41cce36fbfd7017a9c53d45c1ab4d92897385
Lulu <60514664+doclew@users.noreply.github.com>
2022-03-28 10:53:47 +0200

    commit

---
+++ src/components/Events.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="events">
+    <div class="container-fluid">
+      <div class="events-content">
+        <h1 class="title-dbw">Satellite <span>events</span></h1>
+        <a href="#" class="external-btn">
+          <button>
+            <p>See all events</p>
+            <span>
+              <img src="../assets/icon-external-link.svg" alt="" />
+            </span>
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Events",
+};
+</script>
+
+<style>
+</style>
